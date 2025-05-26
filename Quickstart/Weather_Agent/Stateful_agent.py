20d51007768f750aaa52047cf50c1ca3add80efa
A-b-h-a-y-0-2 <abhayabha003@gmail.com>
2025-05-26 10:51:39 +0530

    new stateful weather tool

--- Quickstart/Weather_Agent/Stateful_agent.py
+++ Quickstart/Weather_Agent/Stateful_agent.py
@@ -1,5 +1,6 @@
 # *****Initialize New Session Service and State*****
 from google.adk.sessions import InMemorySessionService
+from google.adk.tools.tool_context import ToolContext
 import warnings 
 import logging
 warnings.filterwarnings("ignore")
@@ -31,8 +32,50 @@ async def main():
         print(retrieved_session.state)
     else:
         print("ERROR: Session not found.")
+        
+    
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     import asyncio
+#     asyncio.run(main())
+def get_weather_stateful(city: str, tool_context: ToolContext)-> dict:
+    """ Retrieves weather information, converts units based on user preference and based on session state"""
+    print(f"---TOOL: get_weather_stateful called for {city}")
+    preferred_unit = tool_context.state.get("user_preference_unit", "Celsius")
+    print(f"---Tool: Reading state 'user_preference_unit' : {preferred_unit}")
+    
+    city_normalized = city.lower().replace(" ","_")
+    
+    mock_weather_db = {
+        "new_york":{"temperature_celsius": 22, "condition": "sunny"},
+        "london":{"temperature_celsius": 25, "condition": "windy"},
+        "tokyo":{"temperature_celsius": 30, "condition": "rainy"},
+    }
+    
+    if city_normalized in mock_weather_db:
+        weather_data = mock_weather_db[city_normalized]
+        temp_c = weather_data["temperature_celsius"]
+        condition = weather_data["condition"]
+        
+        if preferred_unit == "Fahrenheit":
+            temp_value =(9/5) * temp_c + 32
+            temp_unit = "°F"
+            
+        else:
+            temp_value = temp_c
+            temp_unit = "°C"
+        
+        report = f"The weather in {city.capitalize()} is {condition} with a temperture {temp_value}{temp_unit}"
+        result = {"status": "success", "report": report}
+        print(f"---Tool: Generated report in {preferred_unit}. Result: {result}---")
+        
+        tool_context.state["last_city_checked_stateful"] = city
+        print(f"---Tool: Updated session state with last city checked: {city}---")
+        
+        return result
+    else:
+        error_message = f"City {city} not found in mock database."
+        print(f"---Tool: {error_message}---")
+        return {"status": "error", "message": error_message}
+print("State-aware 'get_weather_stateful' tool defined.")
 
